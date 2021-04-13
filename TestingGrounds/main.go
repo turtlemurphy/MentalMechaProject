4476cf66eb26419afdf8985f64eda077be732f66
turtlemurphy <turtlemurphy@gmail.com>
2021-04-13 00:27:12 -0500

    third test commit

--- TestingGrounds/main.go
+++ TestingGrounds/main.go
@@ -6,4 +6,10 @@ func main() {
 	fmt.Println("Hello World")
 	
 	//new test for commit effectivness 
+	
+	// to add new code use these 3 lines
+	// git add .
+	// git commit -m "COMMIT MESSAGE TEXT"
+	// git push -u origin main 
+	
 }
